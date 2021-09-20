a1d225d5bae6eda6c8cc236d2ba0a9b14e113f40
samTech85 <sbonheur85@gmail.com>
2021-09-19 21:43:37 -0400

    getting some info

---
+++ exchange.py
@@ -0,0 +1,12 @@
+'''
+Getting info
+
+1 Chinese Yuan Renminbi (CNY) = 15.0789 HTG
+
+1 USD = 97.5098 HTG
+
+ Euro: Achat 113.3456 / Vente 113.4478
+ Peso dom.: Achat 1.6976 / Vente 1.6992
+ Dollar can.: Achat 75.9481 / Vente 76.0166
+
+ '''
